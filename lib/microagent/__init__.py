0dd6867903b91d9a6c22d450f89959cab6d16fba
Trent Zock-Robbins <robbintt@gmail.com>
2025-12-26 15:28:51 -0800

    Reorganize library.

--- lib/microagent/__init__.py
+++
@@ -1,5 +0,0 @@
-from .microagent import MicroAgent
-from .microagent_executor import MicroAgentExecutor
-from .protocols import ExecutionHarness
-
-__all__ = ["MicroAgent", "MicroAgentExecutor", "ExecutionHarness"]
---
+++ lib/microagent/microagent/__init__.py
@@ -0,0 +1,5 @@
+from .microagent import MicroAgent
+from .microagent.microagent_executor import MicroAgentExecutor
+from .microagent.protocols import ExecutionHarness
+
+__all__ = ["MicroAgent", "MicroAgentExecutor", "ExecutionHarness"]
